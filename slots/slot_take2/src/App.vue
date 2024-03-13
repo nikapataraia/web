ce544f0a10c7b190bee5db06f8462867dacbeb4d
nikapataraia <47186478+nikapataraia@users.noreply.github.com>
2024-03-13 13:23:52 +0400

    ს

--- slots/slot_take2/src/App.vue
+++ slots/slot_take2/src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { generateanimationsets } from './assets/Data_textures';
-import { loadassets , slotTextures } from './assets/Data_textures';
+import { loadassets  } from './assets/Data_textures';
 import GameContainer from './components/GameContainer.vue'
-import * as PIXI from 'pixi.js'
 loadassets()
 generateanimationsets()
 </script>
+
 
 <template>
   <header>
